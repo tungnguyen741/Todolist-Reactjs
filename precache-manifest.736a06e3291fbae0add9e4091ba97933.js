9074d7be3c35ba3ac13c4fa86266769b59990fe8
tungnguyen741 <tung.nguyen21098@gmail.com>
2020-08-23 11:20:56 +0700

    Updates

---
+++ precache-manifest.736a06e3291fbae0add9e4091ba97933.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57711bedc662251cab4fc314c51d635e",
+    "url": "/Todolist-Reactjs/index.html"
+  },
+  {
+    "revision": "4b3143a1c6e0a986b321",
+    "url": "/Todolist-Reactjs/static/css/main.2365943c.chunk.css"
+  },
+  {
+    "revision": "153839053677902db24f",
+    "url": "/Todolist-Reactjs/static/js/2.0f22d19b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todolist-Reactjs/static/js/2.0f22d19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b3143a1c6e0a986b321",
+    "url": "/Todolist-Reactjs/static/js/main.774f3dae.chunk.js"
+  },
+  {
+    "revision": "c4c8d684eb47a405d8f3",
+    "url": "/Todolist-Reactjs/static/js/runtime-main.4c1d7d7c.js"
+  },
+  {
+    "revision": "441d8611106ab6473ed8c5487cda63ac",
+    "url": "/Todolist-Reactjs/static/media/cross.441d8611.svg"
+  },
+  {
+    "revision": "3fc898075b85bba3248d2e735ffece52",
+    "url": "/Todolist-Reactjs/static/media/down-arrow.3fc89807.svg"
+  },
+  {
+    "revision": "739cdfaef4c2230cb36bcf17baae7a05",
+    "url": "/Todolist-Reactjs/static/media/test.739cdfae.svg"
+  },
+  {
+    "revision": "b649c651d0f7a56f122d92c731089a10",
+    "url": "/Todolist-Reactjs/static/media/tick.b649c651.svg"
+  },
+  {
+    "revision": "40eb9cc6d51d63d1dfd8a0ea29b8d700",
+    "url": "/Todolist-Reactjs/static/media/untick.40eb9cc6.svg"
+  }
+]);
